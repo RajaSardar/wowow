5eb207c2fdd525893361d66fb0a77fe4afc2c088
Raja Sardar <rj.raj@outlook.com>
2021-02-24 13:23:30 +0530

    init

--- server.js
+++ server.js
@@ -17,6 +17,5 @@ app.use(adminRoute);
 app.use(userRoute);
 
 app.listen(process.env.PORT || 5000, function () {
-    var port = server.address().port;
-    console.log("Express is working on port " + port);
+    console.log("Express is working on port ");
 });
